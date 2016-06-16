d025a2f2606656958bc1f4f3da222b2f474393a6
usuario <usuario@not>
2016-06-15 23:04:52 -0300

    Terceira versão do Acessa IFSP

--- schema/bd.sql
+++ schema/bd.sql
@@ -1,8 +1,33 @@
---------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 04-Jun-2016 às 22:52
+-- Versão do servidor: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `controle`
+--
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `aluno`
+--
 
 CREATE TABLE `aluno` (
-  `prontuario` int(11) PRIMARY KEY NOT NULL,
+  `prontuario` int(11) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `curso` varchar(200) NOT NULL,
   `endereco` varchar(200) DEFAULT NULL,
@@ -14,6 +39,15 @@ CREATE TABLE `aluno` (
   `foto` varchar(300) DEFAULT NULL,
   `cartao` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `aluno`
+--
+
+INSERT INTO `aluno` (`prontuario`, `nome`, `curso`, `endereco`, `cidade`, `estado`, `telefone`, `celular`, `email`, `foto`, `cartao`) VALUES
+(1420348, 'Eliene', 'fghjkl', 'ghjkl', 'ghjkl', 'gh', 951, 851, 'okm', '', ''),
+(1420364, 'Samuel', 'yuio', 'bnm,', 'uio', 'bn', 952, 52, 'okjhb', '', '');
+
 -- --------------------------------------------------------
 
 --
@@ -21,8 +55,48 @@ CREATE TABLE `aluno` (
 --
 
 CREATE TABLE `registro` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `dado` varchar(250) NOT NULL,
   `hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `registro`
+--
+
+INSERT INTO `registro` (`id`, `dado`, `hora`) VALUES
+(35, ' 61-9000034-01\n', '2016-06-03 23:29:36'),
+(36, ' 61-9000034-01\n', '2016-06-03 23:29:41'),
+(37, ' 61-9000034-01\n', '2016-06-03 23:29:47'),
+(38, ' 61-9000034-01\n', '2016-06-03 23:29:52'),
+(39, '1420364\n', '2016-06-04 14:01:00'),
+(40, ' 1420364\n', '2016-06-04 14:01:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`prontuario`);
+
+--
+-- Indexes for table `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
