a50977494ab17987752edc61a835f2b4a7a15575
a1420267 <a1420267@192.168.130.12>
2016-06-17 20:12:27 -0300

    Arquivo com foto

--- AcessaIFSP/cadastrar_aluno.php
+++ AcessaIFSP/cadastrar_aluno.php
@@ -16,15 +16,17 @@ $mail = $_POST['mail'];
 
 
 
-
+//upload de foto
 $uploaddir = 'img/avatar/';
 
 $uploadfile = $uploaddir . $_FILES['foto']['name'];
 
-if (move_uploaded_file($_FILES['foto']['tmp_name'], $uploadfile)){
-echo "Arquivo Enviado";}
-else {echo "Arquivo não enviado";}
-
+/*if (move_uploaded_file($_FILES['foto']['tmp_name'], $uploadfile)) {
+    echo "Arquivo Enviado";
+} else {
+    echo "Arquivo não enviado";
+}*/
+//fim upload de foto
 
 
 
